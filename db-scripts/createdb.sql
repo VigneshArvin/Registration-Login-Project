8ef493077db4082fcb0e3402f53c090096bf516f
Arvin Prime <47138856+VigneshArvin@users.noreply.github.com>
2021-09-29 00:10:09 +0530

    Delete db-scripts directory

--- db-scripts/createdb.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'employee-data'@'localhost' IDENTIFIED BY 'employee-data';
-GRANT ALL PRIVILEGES ON * . * TO 'EmployeeData'@'localhost';
-ALTER USER 'employee-data'@'localhost' IDENTIFIED WITH mysql_native_password BY 'employee-data';
